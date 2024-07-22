0a53206e204045a7c826d0240a7e38a8ebd7c3b3
Alexandre Beduc <alexandre.beduc@gmail.com>
2024-07-22 09:47:46 +0200

    added a functional memory, moved the current position from Gui.py to the class RumorGenerator

--- src/extractor.py
+++ src/extractor.py
@@ -1,5 +1,5 @@
 import csv
-from typing import Optional, Dict
+from typing import Optional, Dict, List
 import random
 import os
 
@@ -27,83 +27,73 @@ class Rumor:
 
 class RumorMemory:
     def __init__(self, init_file: Optional[str] = None):
-        self.memory: Dict[str, Rumor] = {}
+        self.memory: List[str] = []
+        self.save_file = init_file
+        self.load_memory()
 
     def load_memory(self):
-        pass
+        with open(self.save_file, "r", encoding="utf-8") as csv_file:
+            csv_data = csv.reader(csv_file, delimiter=",")
+            for row in csv_data:
+                self.memory.append(row[0])
 
-    def add_to_memory(self):
-        pass
+    def add_to_memory(self, rumor_key):
+        self.memory.append(rumor_key)
 
     def save_memory(self):
-        pass
+        with open(self.save_file, "w", encoding="utf-8", newline="") as csv_file:
+            csv_writer = csv.writer(csv_file)
+            for elem in self.memory:
+                csv_writer.writerow([elem])
 
     def __repr__(self) -> str:
-        pass
+        return f"{self.memory}\n"
 
 
 class RumorGenerator:
-    def __init__(self, init_file_path: Optional[str] = None, current_memory=None):
+    def __init__(self, init_file_path: Optional[str] = None, memory_file_path=None):
         self.init_file_path = init_file_path
+        self.memory_file_path = memory_file_path
+        self.rumor_memory: RumorMemory = RumorMemory(self.memory_file_path)
         self.rumors: Dict[str, Rumor] = self.init_rumors()
-        self.rumor_memory: RumorMemory = RumorMemory()
-        self.current_memory = current_memory
+        self.current_rumor = None
 
     def init_rumors(self) -> Dict[str, Rumor]:
         rumors: Dict[str, Rumor] = {}
-        try:
-            with open(self.init_file_path, "r", encoding="utf-8") as csvfile:
-                csv_data = csv.reader(csvfile, delimiter=",")
-                for row in csv_data:
-                    if row[0] == "rumor_id":
-                        continue
-                    rumor = Rumor()
-                    rumor.rumor_id = row[0]
-                    rumor.rumor_subject = row[1]
-                    rumor.rumor_title = row[2]
-                    rumor.rumor_text = row[3]
-                    rumor.tag_boi = row[4]
-                    rumor.tag_hou = row[5]
-                    rumor.tag_bre = row[6]
-                    rumor.tag_bry = row[7]
-                    rumor.tag_din = row[8]
-                    rumor.tag_kon = row[9]
-                    rumor.tag_hav = row[10]
-                    rumor.tag_tar = row[11]
-                    rumor.tag_ter = row[12]
-                    rumor.tag_dou = row[13]
-                    rumors[rumor.rumor_id] = rumor
-            return rumors
-        except FileNotFoundError:
-            print(f"File not found at {self.init_file_path}")
-            return rumors
+        with open(self.init_file_path, "r", encoding="utf-8") as csvfile:
+            csv_data = csv.reader(csvfile, delimiter=",")
+            for row in csv_data:
+                if row[0] == "rumor_id":
+                    continue
+                rumor = Rumor()
+                rumor.rumor_id = row[0]
+                rumor.rumor_subject = row[1]
+                rumor.rumor_title = row[2]
+                rumor.rumor_text = row[3]
+                rumor.tag_boi = row[4]
+                rumor.tag_hou = row[5]
+                rumor.tag_bre = row[6]
+                rumor.tag_bry = row[7]
+                rumor.tag_din = row[8]
+                rumor.tag_kon = row[9]
+                rumor.tag_hav = row[10]
+                rumor.tag_tar = row[11]
+                rumor.tag_ter = row[12]
+                rumor.tag_dou = row[13]
+                rumors[rumor.rumor_id] = rumor
+        self.remove_saved_rumors(rumors)
+        return rumors
+
+    def remove_saved_rumors(self, rumors):
+        for rumor_key in self.rumor_memory.memory:
+            if rumor_key in rumors:
+                rumors.pop(rumor_key)
+
+    def update_memory(self):
+        self.rumor_memory.add_to_memory(self.current_rumor.rumor_id)
 
     def get_rumor_by_id(self, rumor_id: str) -> Optional[Rumor]:
         return self.rumors[rumor_id]
 
     def get_random_rumor(self) -> Rumor:
-        return self.get_rumor_by_id(random.choice(list(self.rumors.keys())))
-
-
-def main():
-    data_directory = os.path.join(os.path.dirname(__file__), "..", "data", "general_rumor.csv")
-    extractor = RumorGenerator(init_file_path=data_directory)
-    # for rumor_id, rumor in extractor.rumors.items():
-    # print(f"ID_key: {rumor_id},\n "
-    #       f"ID_Obj: {rumor.rumor_id},\n"
-    #       f"Title: {rumor.rumor_title},\n")
-
-    # print(extractor.rumors)
-    rumor_a001 = extractor.get_rumor_by_id(rumor_id="A003")
-    print(rumor_a001)
-    rumor_a002 = extractor.get_rumor_by_id(rumor_id="A002")
-    print(rumor_a002.rumor_id)
-    print("------")
-    random_rumorx = extractor.get_random_rumor()
-    print(random_rumorx.rumor_id)
-    print(random_rumorx.rumor_title)
-    print(random_rumorx.rumor_text)
-
-
-if __name__ == "__main__":
-    main()
+        self.current_rumor = self.get_rumor_by_id(random.choice(list(self.rumors.keys())))
