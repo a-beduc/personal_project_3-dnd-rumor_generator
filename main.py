3cc46d95f467fb22bc948f62beb6a3de115fd4a6
Alexandre Beduc <alexandre.beduc@gmail.com>
2024-07-22 09:48:01 +0200

    added a path to the memory file

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from src.gui import start_gui
 
 def main():
     data_directory = os.path.join(os.path.dirname(__file__), "data", "general_rumor.csv")
-    start_gui(data_directory)
-
+    memory_directory = os.path.join(os.path.dirname(__file__), "memory", "memory.csv")
+    start_gui(data_directory, memory_directory)
 
 
 if __name__ == '__main__':
