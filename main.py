5e8f0d74b2c42498af21bd949696b48bf62bddae
Alexandre Beduc <alexandre.beduc@gmail.com>
2024-07-23 10:09:08 +0200

    Added comment to main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from src.gui import start_gui
 
 
 def main():
+    """Data directory and memory directory are csv file containing respectively every rumors and id of rumors saved in
+        memory. These files should be replaced by a Database in the future"""
     data_directory = os.path.join(os.path.dirname(__file__), "data", "general_rumor.csv")
     memory_directory = os.path.join(os.path.dirname(__file__), "memory", "memory.csv")
     start_gui(data_directory, memory_directory)
