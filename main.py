8952461a767f1419f37ff5149a4e6b60bdb4c8a7
Alexandre Beduc <alexandre.beduc@gmail.com>
2024-07-25 10:31:35 +0200

    removed pathing from main and added it to GUI instead

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import os
 from src.gui import start_gui
 
 
 def main():
-    """Data directory and memory directory are csv file containing respectively every rumors and id of rumors saved in
-        memory. These files should be replaced by a Database in the future"""
-    data_directory = os.path.join(os.path.dirname(__file__), "data", "general_rumor.csv")
-    memory_directory = os.path.join(os.path.dirname(__file__), "memory", "memory.csv")
-    start_gui(data_directory, memory_directory)
+    start_gui()
 
 
 if __name__ == '__main__':
